7e072580103778718e9ea6eb05a5b1de9d2e53fe
Thijs Busser <tbusser@gmail.com>
2024-11-19 11:32:40 +0100

    Add solution for 2015, day 5

---
+++ 2015/05/part-1.ts
@@ -0,0 +1,47 @@
+const doubleLetterRegex = /([a-z])\1/;
+const forbiddenCombosRegex = /ab|cd|pq|xy/;
+const vowelRegex = /[aeiou]/g;
+
+/* ========================================================================== */
+
+export function testDoubleLetter(word: string): boolean {
+	return doubleLetterRegex.test(word);
+}
+
+export function testNoForbiddenCombos(word: string): boolean {
+	return !forbiddenCombosRegex.test(word);
+}
+
+export function testVowelCondition(word: string): boolean {
+	return (word.match(vowelRegex)?.length ?? 0) >= 3;
+}
+
+export function isWordNice(word: string): boolean {
+	return (
+		testVowelCondition(word) &&
+		testDoubleLetter(word) &&
+		testNoForbiddenCombos(word)
+	);
+}
+
+/* ========================================================================== */
+
+async function solver(input: string): Promise<number> {
+	const words = input.split('\n');
+	let numberOfNiceWords = 0;
+
+	words.forEach(word => {
+		if (isWordNice(word)) {
+			numberOfNiceWords++;
+		}
+	});
+
+	return numberOfNiceWords;
+}
+
+/* ========================================================================== */
+
+export default {
+	prompt: 'Number of nice words',
+	solver
+} satisfies Solution;
---
+++ 2015/05/part-2.ts
@@ -0,0 +1,46 @@
+export function testRepeatingLetter(word: string): boolean {
+	for (let index = 0; index < word.length - 2; index++) {
+		const isRepeated = word.charAt(index) === word.charAt(index + 2);
+
+		if (isRepeated) return true;
+	}
+
+	return false;
+}
+
+export function testRepeatingGroup(word: string): boolean {
+	for (let index = 0; index < word.length - 3; index++) {
+		const group = word.substring(index, index + 2);
+		const isRepeated = word.indexOf(group, index + 2) !== -1;
+
+		if (isRepeated) return true;
+	}
+
+	return false;
+}
+
+function isWordNice(word: string): boolean {
+	return testRepeatingGroup(word) && testRepeatingLetter(word);
+}
+
+/* ========================================================================== */
+
+async function solver(input: string): Promise<number> {
+	const words = input.split('\n');
+	let numberOfNiceWords = 0;
+
+	words.forEach(word => {
+		if (isWordNice(word)) {
+			numberOfNiceWords++;
+		}
+	});
+
+	return numberOfNiceWords;
+}
+
+/* ========================================================================== */
+
+export default {
+	prompt: 'Number of nice words',
+	solver
+} satisfies Solution;
