c8f15351ee55a49861a6b3f708fdb3242a88f4b5
Thijs Busser <tbusser@gmail.com>
2024-11-19 09:46:53 +0100

    Add scaffolding
    Add the configuration for the environment to solve the advent of
    code puzzles.

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	{ files: ['**/*.{js,mjs,cjs,ts}'] },
+	{ languageOptions: { globals: globals.browser } },
+
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+
+	{
+		plugins: { '@stylistic': stylistic },
+		rules: {
+			// This rule enforces at least one newline at the end of
+			// non-empty files.
+			'@stylistic/eol-last': ['error', 'always'],
+
+			// This rule enforces a consistent indentation style.
+			'@stylistic/indent': ['error', 'tab'],
+
+			'@stylistic/max-len': ['error', { code: 120, comments: 80 }],
+
+			// This rule enforces the consistent use of single quotes.
+			'@stylistic/quotes': ['error', 'single'],
+
+			// This rule enforces consistent use of semicolons.
+			'@stylistic/semi': ['error', 'always']
+		}
+	}
+];
---
+++ scripts/run-solution.ts
@@ -0,0 +1,53 @@
+import { existsSync } from 'fs';
+import path from 'path';
+
+import { solve } from './utils/runner.js';
+
+/* ========================================================================== */
+
+function validatePath(puzzlePath: string, errorMessage: string) {
+	if (!existsSync(puzzlePath)) {
+		console.log(errorMessage);
+		process.exit(1);
+	}
+}
+
+/* ========================================================================== */
+
+// The second argument is the puzzle ID, this should be in the
+// format <year>/<day>/<part>. Make sure an argument is provided before trying
+// to validate the argument.
+const puzzleId = process.argv[2];
+if (puzzleId === undefined) {
+	console.log('Please provide the puzzle ID as <year>/<day>/<part>.');
+	process.exit(1);
+}
+
+// Split up the argument into its parts and convert them to numbers.
+const [year, day, part] = puzzleId.split('/').map(Number);
+
+// Make sure there is a matching year folder in the current directory.
+const yearFolder = path.relative(process.cwd(), year.toString());
+validatePath(yearFolder, `Year ${year} does not exist, please provide a valid year.`);
+
+// Make sure there is a matching day folder in the year folder, days should
+// always be two digits long.
+const paddedDay = day.toString().padStart(2, '0');
+const dayFolder = path.join(yearFolder, paddedDay);
+validatePath(
+	dayFolder,
+	`Day ${paddedDay} does not exist within ${year}, please provide a valid day.`
+);
+
+// Make sure there is a matching part file in the day folder, parts should
+// always be one digit long.
+const fileName = path.join(dayFolder, `part-${part}.ts`);
+validatePath(fileName, `Part ${part} does not exist in ${dayFolder}, please provide a valid part.`);
+
+// Start the script for the provided puzzle ID.
+const { default: solution } = await import(path.resolve(process.cwd(), fileName));
+solve({
+	day,
+	year,
+	...solution
+});
