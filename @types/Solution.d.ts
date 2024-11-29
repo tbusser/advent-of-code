01bd721a68b02ffdebc8f2109dda9d7d2f6a9ac9
Thijs Busser <tbusser@gmail.com>
2024-11-29 16:32:13 +0100

    Add JSDoc comments to the Solution type
    Comments for the two properties have been added.

--- @types/Solution.d.ts
+++ @types/Solution.d.ts
@@ -1,4 +1,15 @@
 declare type Solution<T = number> = {
+	/**
+	 * The text to describe what the answer represents. It will automatically
+	 * be given colon (":") at the end, just before the answer.
+	 */
 	prompt: string;
+
+	/**
+	 * The method which will be given the puzzle input and is expected to return
+	 * the answer.
+	 *
+	 * @param input The puzzle input from the input.txt file.
+	 */
 	solver: (input: string) => Promise<T>;
 };
