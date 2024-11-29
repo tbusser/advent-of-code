847ac16ed47bb011da74328e57678cc40b45ed74
Thijs Busser <tbusser@gmail.com>
2024-11-29 16:22:38 +0100

    Update max-len rule
    Lines which run long because they have a long string template literal
    should not be considered to be in violation of the rules. Often these
    string can't be made shorter or wrapped over multiple lines.

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import stylistic from '@stylistic/eslint-plugin';
 
+/* ========================================================================== */
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -22,11 +23,18 @@ export default [
 			// This rule enforces a consistent indentation style.
 			'@stylistic/indent': ['error', 'tab'],
 
-			'@stylistic/max-len': ['error', { code: 120, comments: 80, ignoreUrls: true }],
+			'@stylistic/max-len': ['error', {
+				code: 120,
+				comments: 80,
+				ignoreTemplateLiterals: true,
+				ignoreUrls: true
+			}],
 
-			'@stylistic/padding-line-between-statements': ['error',
-				{ blankLine: 'always', prev: '*', next: 'return' }
-			],
+			'@stylistic/padding-line-between-statements': ['error', {
+				blankLine: 'always',
+				next: 'return',
+				prev: '*',
+			}],
 
 			// This rule enforces the consistent use of single quotes.
 			'@stylistic/quotes': ['error', 'single'],
