c16816bdf6cbfe31eb746a10c92e8dfdf5976fb0
Thijs Busser <tbusser@gmail.com>
2024-12-02 08:41:06 +0100

    Fix error in solution 2024, day 2
    Forgot to save my changes before committing it. Extracted the method
    to check if a report is safe but the changes in part 1 to use this
    extracted method weren't saved.

--- 2024/02/helpers/is-report-safe.ts
+++ 2024/02/helpers/is-report-safe.ts
@@ -7,13 +7,13 @@ export function isReportSafe(report: Report): boolean {
 	// be negative. Same for when the first delta is positive, they all should
 	// be positive.
 	const deltaCheck = report[0] < report[1]
-		? (difference) => difference < 0
-		: (difference) => difference > 0;
+		? (difference: number) => difference < 0
+		: (difference: number) => difference > 0;
 
 	for (let index = 0; index < report.length - 1; index++) {
 		// Calculating the difference per check is faster than calculating it
 		// once and storing it in a const.
-		if (Math.abs(report[index] - report[index + 1]) < 0) return false;
+		if (Math.abs(report[index] - report[index + 1]) < 1) return false;
 		if (Math.abs(report[index] - report[index + 1]) > 3) return false;
 		if (!deltaCheck(report[index] - report[index + 1])) return false;
 	}
--- 2024/02/part-1.ts
+++ 2024/02/part-1.ts
@@ -1,22 +1,7 @@
-import { parseInput, Report } from './helpers/parse-input.js';
+import { isReportSafe } from './helpers/is-report-safe.js';
+import { parseInput } from './helpers/parse-input.js';
 
 /* ========================================================================== */
-
-function isReportSafe(report: Report): boolean {
-	const directionCheck = report[0] < report[1]
-		? (difference) => difference < 0
-		: (difference) => difference > 0;
-
-	for (let index = 0; index < report.length - 1; index++) {
-		const difference = report[index] - report[index + 1];
-		if (Math.abs(difference) < 0 || Math.abs(difference) > 3) return false;
-		if (!directionCheck(difference)) return false;
-	}
-
-	return true;
-}
-
-/* -------------------------------------------------------------------------- */
 
 async function solver(input: string): Promise<number> {
 	const reports = parseInput(input);
