e56c0947f8b45d3187b60c310981f7bbc285d93f
Thijs Busser <tbusser@gmail.com>
2025-01-07 09:12:35 +0100

    Refactor key from string to number
    Having to do so many string concats to create the keys takes a very
    long time making the entire solution quite slow. By using a number as
    the key it speeds up the solution considerably. Solution inspired by
    Rob Habraken.

--- 2024/22/part-2.ts
+++ 2024/22/part-2.ts
@@ -5,12 +5,12 @@ import { parseInput } from './helpers/parse-input.js';
 
 function solver(input: string): number {
 	const buyers = parseInput(input);
-	const deltaGroups = new Map<string, number>();
+	const deltaGroups = new Map<number, number>();
 
 	for (const secret of buyers) {
 		let previousPrice = secret % 10;
 		const deltas: number[] = [];
-		const buyerPairs = new Set<string>();
+		const buyerPairs = new Set<number>();
 
 		for (let index = 0, value = secret; index < 2000; index++) {
 			// Generate the next secret.
@@ -26,8 +26,17 @@ function solver(input: string): number {
 			// means it is possible to create a group of 4 deltas and see how
 			// many bananas it will give us.
 			if (index >= 3) {
-				// Create a key for this group of price deltas.
-				const key = `${deltas[index - 3]},${deltas[index - 2]},${deltas[index - 1]},${deltas[index]}`;
+				// Using a string as a key is very slow, using a number is a lot
+				// faster. Since the maximum change is from -9 to 9 the numbers
+				// in the deltas array will never exceed 18. By multiplying the
+				// values with the powers of 18 we can ensure the resulting
+				// number is unique for that set of price changes.
+				const key =
+					(deltas[index - 3] * 5832) + // 18^3=5832
+					(deltas[index - 2] * 324) +  // 18^2=324
+					(deltas[index - 1] * 18) +   // 18^1=18
+					deltas[index];               // 18^0=1
+
 				// Check if this combination of price deltas has not yet been
 				// encountered. We only want to process the value of the first
 				// time a key is encountered per buyer.
